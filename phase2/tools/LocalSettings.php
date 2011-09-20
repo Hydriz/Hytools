0983e563c209aee5701f07669c7a10099a6d1248
Hydriz <admin@wikisorg.tk>
2011-09-20 07:08:43 +0000

    Importing from alternate repository

---
+++ phase2/tools/LocalSettings.php
@@ -0,0 +1,7 @@
+<?php
+
+/*
+	Get the settings from the original file
+*/
+
+require_once("/home/hydriz/public_html/ssl/LocalSettings.php");
---
+++ phase3/includes/RightNavigation.php
@@ -0,0 +1,39 @@
+<div id="p-views" class="vectorTabs"> 
+	<h5>Views</h5> 
+	<ul> 
+					<li id="ca-view" class="selected"><span><a href="/w/index.php/Main_Page" >Read</a></span></li> 
+					<li id="ca-edit"><span><a href="/w/index.php?title=Main_Page&amp;action=edit"  title="You can edit this page. Please use the preview button before saving [e]" accesskey="e">Edit</a></span></li> 
+					<li id="ca-history" class="collapsible "><span><a href="/w/index.php?title=Main_Page&amp;action=history"  title="Past revisions of this page [h]" accesskey="h">View history</a></span></li> 
+					<li id="ca-watch" class="icon"><span><a href="/w/index.php?title=Main_Page&amp;action=watch"  title="Add this page to your watchlist [w]" accesskey="w">Watch</a></span></li> 
+			</ul> 
+</div> 
+ 
+<!-- /0 --> 
+ 
+<!-- 1 --> 
+<div id="p-cactions" class="vectorMenu"> 
+	<h5><span>Actions</span><a href="#"></a></h5> 
+	<div class="menu"> 
+		<ul> 
+							<li id="ca-delete"><a href="/w/index.php?title=Main_Page&amp;action=delete"  title="Delete this page [d]" accesskey="d">Delete</a></li> 
+							<li id="ca-move"><a href="/w/index.php/Special:MovePage/Main_Page"  title="Move this page [m]" accesskey="m">Move</a></li> 
+							<li id="ca-protect"><a href="/w/index.php?title=Main_Page&amp;action=protect"  title="Protect this page [=]" accesskey="=">Protect</a></li> 
+					</ul> 
+	</div> 
+</div> 
+ 
+<!-- /1 --> 
+ 
+<!-- 2 --> 
+<div id="p-search"> 
+	<h5><label for="searchInput">Search</label></h5> 
+	<form action="/w/index.php" id="searchform"> 
+		<input type='hidden' name="title" value="Special:Search"/> 
+				<div id="simpleSearch"> 
+						<input id="searchInput" name="search" type="text"  title="Search Wikipedia [f]" accesskey="f"  value="" /> 
+						<button id="searchButton" type='submit' name='button'  title="Search the pages for this text"><img src="/w/skins/vector/images/search-ltr.png?301" alt="Search" /></button> 
+					</div> 
+			</form> 
+</div> 
+ 
+<!-- /2 --> 
