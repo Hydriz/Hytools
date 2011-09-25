b986f7548558a0b64b313de4edc537a9a001de5c
Hydra <admin@wikisorg.tk>
2011-09-25 22:45:19 +0800

    Lets try a rewrite

--- private/listfiles.php
+++ private/listfiles.php
@@ -1,33 +1,15 @@
 <?php
 
-  function getDirectoryList ($directory) 
-  {
-
-    // create an array to hold directory list
-    $results = array();
-
-    // create a handler for the directory
-    $handler = opendir($directory);
-
-    // open directory and walk through the filenames
-    while ($file = readdir($handler)) {
-
-      // if file isn't this directory or its parent, add it to the results
-      if ($file != "." && $file != "..") {
-        $results[] = $file;
-      }
-
-    }
-
-    // tidy up: close the handler
-    closedir($handler);
-
-    // done!
-    return $results;
-
-  }
-
-// The epic test
-getDirectoryList(/home/hydriz/public_html);
+//path to directory to scan
+$directory = "/mnt/user-store/xmldumps/";
+ 
+//get all image files with a .jpg extension.
+$images = glob($directory);
+ 
+//print each file name
+foreach($images as $image)
+{
+echo $image;
+}
 
 ?>
