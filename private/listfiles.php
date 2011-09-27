68e6048d21c0e9732af37107f54c088b2622748e
Hydra <admin@wikisorg.tk>
2011-09-27 18:05:43 +0800

    Grouping wikis alphabetically

--- private/listfiles.php
+++ private/listfiles.php
@@ -33,9 +33,135 @@ function ListFiles($dir) {
 }
 
 
-foreach (ListFiles('/mnt/user-store/xmldumps') as $key=>$file){
-    echo '<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+foreach (ListFiles('/mnt/user-store/xmldumps/a') as $key=>$file){
+    echo '<h2>a</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
 }  
+
+foreach (ListFiles('/mnt/user-store/xmldumps/b') as $key=>$file){
+    echo '<h2>b</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/c') as $key=>$file){
+    echo '<h2>c</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/d') as $key=>$file){
+    echo '<h2>d</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/e') as $key=>$file){
+    echo '<h2>e</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/f') as $key=>$file){
+    echo '<h2>f</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/g') as $key=>$file){
+    echo '<h2>g</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/h') as $key=>$file){
+    echo '<h2>h</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/i') as $key=>$file){
+    echo '<h2>i</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/j') as $key=>$file){
+    echo '<h2>j</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/k') as $key=>$file){
+    echo '<h2>k</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/l') as $key=>$file){
+    echo '<h2>l</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/m') as $key=>$file){
+    echo '<h2>m</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/n') as $key=>$file){
+    echo '<h2>n</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/o') as $key=>$file){
+    echo '<h2>o</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/p') as $key=>$file){
+    echo '<h2>p</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/q') as $key=>$file){
+    echo '<h2>q</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/r') as $key=>$file){
+    echo '<h2>r</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/s') as $key=>$file){
+    echo '<h2>s</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/t') as $key=>$file){
+    echo '<h2>t</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/u') as $key=>$file){
+    echo '<h2>u</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/v') as $key=>$file){
+    echo '<h2>v</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/w') as $key=>$file){
+    echo '<h2>w</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/x') as $key=>$file){
+    echo '<h2>x</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/y') as $key=>$file){
+    echo '<h2>y</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
+
+foreach (ListFiles('/mnt/user-store/xmldumps/z') as $key=>$file){
+    echo '<h2>z</h2>
+<li><a href="download.php?download_file=' . $file . '">' . $file . "</a></li>";
+}
 
 echo "</body>
 </html>";
