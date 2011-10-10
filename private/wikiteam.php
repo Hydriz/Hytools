521622143add29b66f5541b07c3cae4f80744114
Hydra <admin@wikisorg.tk>
2011-10-10 23:20:02 +0800

    Adding list of wikis being dumped

--- private/wikiteam.php
+++ private/wikiteam.php
@@ -35,6 +35,15 @@ foreach (ListFiles('/mnt/user-store/wikiteam/public') as $key=>$file){
     echo '<li><a href="down.php?download_file=' . $file . '">' . $file . "</a></li>";
 }
 
+// Dumps in progress, sadly have to manually update
+echo '<h2>Wikis currently in the process of being dumped</h2>
+<p>Below are the wikis that are currently being dumped:</p>
+<li><a href="http://wikidoc.org">wikidoc.org</a></li>
+<li><a href="http://www.omegawiki.org">omegawiki.org</a></li>
+<li><a href="http://www.wikihow.com">wikihow.com</a></li>
+<li><a href="http://www.fanhistory.com/w">fanhistory.com</a></li>
+<li><a href="http://bvio.com">bvio.com</a></li>';
+
 echo "</body>
 </html>";
 
