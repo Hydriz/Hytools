0e7d79e07ed03c6aaab2b407d4954c2895897637
Hydra <admin@alphacorp.tk>
2012-02-24 18:52:39 +0800

    Switching the protocol from http to https for the sidebar

--- ssl/includes/Sidebar.php
+++ ssl/includes/Sidebar.php
@@ -6,7 +6,7 @@
 		<ul> 
 			<li id="n-mainpage-description"><a href="./index.php" title="Visit the main page [z]" accesskey="z">Main page</a></li> 
 			<li id="n-contents"><a href="./matrix.php" title="Account matrix of Hydriz">Account Matrix</a></li> 
-			<li id="n-sitesupport"><a href="http://wikimediafoundation.org/wiki/Special:Landingcheck?landing_page=WMFJA085&amp;language=en&amp;utm_source=donate&amp;utm_medium=sidebar&amp;utm_campaign=20101204SB002" title="Support us">Donate to Wikimedia</a></li> 
+			<li id="n-sitesupport"><a href="https://wikimediafoundation.org/wiki/Special:Landingcheck?landing_page=WMFJA085&amp;language=en&amp;utm_source=donate&amp;utm_medium=sidebar&amp;utm_campaign=20101204SB002" title="Support us">Donate to Wikimedia</a></li> 
 		</ul> 
 	</div> 
 </div> 
@@ -14,9 +14,9 @@
 	<h5>On Wikimedia</h5> 
 	<div class="body"> 
 		<ul> 
-			<li id="n-help"><a href="http://meta.wikimedia.org/wiki/User:Hydriz" title="Hydriz's userpage on Meta Wiki">Hydriz's userpage</a></li> 
-			<li id="n-aboutsite"><a href="http://meta.wikimedia.org/wiki/User_talk:Hydriz" title="Send messages to Hydriz on Meta Wiki">Hydriz's talk page</a></li> 
-			<li id="n-portal"><a href="http://meta.wikimedia.org/wiki/Special:CentralAuth/Hydriz" title="Information of Hydriz's global account using CentralAuth">Global Hydriz</a></li> 
+			<li id="n-help"><a href="https://meta.wikimedia.org/wiki/User:Hydriz" title="Hydriz's userpage on Meta Wiki">Hydriz's userpage</a></li> 
+			<li id="n-aboutsite"><a href="https://meta.wikimedia.org/wiki/User_talk:Hydriz" title="Send messages to Hydriz on Meta Wiki">Hydriz's talk page</a></li> 
+			<li id="n-portal"><a href="https://meta.wikimedia.org/wiki/Special:CentralAuth/Hydriz" title="Information of Hydriz's global account using CentralAuth">Global Hydriz</a></li> 
 		</ul> 
 	</div> 
 </div> 
@@ -24,9 +24,9 @@
 	<h5>Toolbox</h5> 
 	<div class="body"> 
 		<ul> 
-			<li id="t-whatlinkshere"><a href="http://commons.wikimedia.org/wiki/Commons:Upload" title="Upload a file to Wikimedia Commons" accesskey="j">Upload a file to Commons</a></li> 
-			<li id="t-emailuser"><a href="http://meta.wikimedia.org/wiki/Special:EmailUser/Hydriz" title="Send an e-mail to Hydriz on Meta Wiki">E-mail Hydriz</a></li> 
-			<li id="t-upload"><a href="http://svn.wikimedia.org/svnroot/mediawiki/" title="Visit the Subversion repository of Wikimedia" accesskey="u">Wikimedia subversion repository</a></li> 
+			<li id="t-whatlinkshere"><a href="https://commons.wikimedia.org/wiki/Commons:Upload" title="Upload a file to Wikimedia Commons" accesskey="j">Upload a file to Commons</a></li> 
+			<li id="t-emailuser"><a href="https://meta.wikimedia.org/wiki/Special:EmailUser/Hydriz" title="Send an e-mail to Hydriz on Meta Wiki">E-mail Hydriz</a></li> 
+			<li id="t-upload"><a href="https://svn.wikimedia.org/svnroot/mediawiki/" title="Visit the Subversion repository of Wikimedia" accesskey="u">Wikimedia subversion repository</a></li> 
 		</ul> 
 	</div> 
 </div> 
@@ -34,9 +34,9 @@
 	<h5>In Wikipedia</h5> 
 	<div class="body"> 
 		<ul> 
-			<li class="interwiki-en"><a href="http://en.wikipedia.org/wiki/User:Hydriz" title="User:Hydriz">English</a></li>
-			<li class="interwiki-simple"><a href="http://simple.wikipedia.org/wiki/User:Hydriz" title="User:Hydriz">Simple English</a></li> 
-			<li class="interwiki-zh"><a href="http://zh.wikipedia.org/wiki/User:Hydriz" title="User:Hydriz">中文</a></li> 
+			<li class="interwiki-en"><a href="https://en.wikipedia.org/wiki/User:Hydriz" title="User:Hydriz">English</a></li>
+			<li class="interwiki-simple"><a href="https://simple.wikipedia.org/wiki/User:Hydriz" title="User:Hydriz">Simple English</a></li> 
+			<li class="interwiki-zh"><a href="https://zh.wikipedia.org/wiki/User:Hydriz" title="User:Hydriz">中文</a></li> 
 		</ul> 
 	</div> 
 </div> 
